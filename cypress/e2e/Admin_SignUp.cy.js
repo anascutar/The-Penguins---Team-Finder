78a253e035d69885e6df658b2fafe169e97d4ca9
anascutar <ana.scutar@student.usv.ro>
2024-03-15 17:14:34 +0200

    Verify the Sign Up link & Insert text in the fields

--- cypress/e2e/Admin_SignUp.cy.js
+++ cypress/e2e/Admin_SignUp.cy.js
@@ -1,7 +1,21 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Admin Sign Up", () => {
+  beforeEach("Verify the Sign Up link", () => {
     cy.visit(
-      "https://atc-2024-thepenguins-fe-linux-web-app.azurewebsites.net/"
+      "https://atc-2024-thepenguins-fe-linux-web-app.azurewebsites.net/register"
     );
+    cy.url().should(
+      "include",
+      "https://atc-2024-thepenguins-fe-linux-web-app.azurewebsites.net/register"
+    );
+  });
+
+  it("Insert text in the fields", () => {
+    cy.get("#firstName").type("Ana");
+    cy.get("#lastName").type("Scutar");
+    cy.get("#eMailAdress").type("ana.scutar@student.usv.ro");
+    cy.get("#password").type("password123");
+    cy.get("#organisationName").type("Penguins Software");
+    cy.get("#headquarterAddress").type("Str. Sperantei");
+    cy.get(".Button_button__JBBzO").click();
   });
 });
